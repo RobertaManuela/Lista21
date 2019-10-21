e686c0ffb065709907bb1541a4610041a30c7688
Roberta Manuela <robertamanuellyaraujo@hotmail.com>
2019-10-21 14:16:58 -0300

    Lista21

---
+++ Lista21solution/Ex03/Cliente.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex03
+{
+    class Cliente
+    {
+        private List<ContaBancaria> b = new List<ContaBancaria>();
+
+        public string nome { get; set; }
+        private string email { get; set; }
+        private int fone { get; set; }
+
+
+    }
+}
---
+++ Lista21solution/Ex03/ContaBancaria.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex03
+{
+    class ContaBancaria
+    {
+        private int numero;
+        private decimal saldo;
+
+        public ContaBancaria(decimal saldo, int numero)
+        {
+            this.numero = numero;
+            this.saldo = saldo;
+        }
+
+        public void Depositar(decimal m)
+        {
+            if (m > 0) saldo += m;
+            else throw new InversaoSaldoException("Valor inválido");
+
+        }
+
+        public void Sacar(decimal m)
+        {
+            if (m > 0) saldo -= m;
+            else throw new InversaoSaldoException("O valor informado é invalido");
+        }
+
+        public decimal VerificarSaldo()
+        {
+            return saldo;
+        }
+
+    }
+}
