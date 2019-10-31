e864affa184550cb745b89500c85f6a1d6e4929e
Roberta Manuela <robertamanuellyaraujo@hotmail.com>
2019-10-31 15:24:04 -0300

    ex04

---
+++ Lista21solution/Ex04/Jogador.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex04
+{
+    class Jogador : IComparable
+    {
+        private string nome, email;
+        private int PontuacaoMax;
+        private DateTime data;
+
+        public Jogador(string n, string e)
+        {
+            n = nome;
+            e = email;
+        }
+
+        //método compareto
+
+        public void SetPontos(int p, DateTime d)
+        {
+            if (PontuacaoMax > 0) p = PontuacaoMax;
+            else throw new ArgumentOutOfRangeException("Pontuacao Inválida");
+
+            if (data < DateTime.Now) d = data;
+            else throw new ArgumentOutOfRangeException("Data Inválida");
+        }
+
+        public override string ToString()
+        {
+            return $"{nome} - {email} - {PontuacaoMax} - {data}";
+        }
+
+        public string GetEmail()
+        {
+            return email;
+        }
+    }
+}
---
+++ Lista21solution/Ex04/Jogo.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex04
+{
+    class Jogo
+    {
+        private string nome;
+        List<Jogador> jogs = new List<Jogador>();
+
+        public Jogo(string n)
+        {
+            n = nome;
+        }
+
+        public void Inserir(Jogador j)
+        {
+
+            foreach (Jogador m in jogs)
+            {
+                if (m.GetEmail == j.GetEmail) throw new EmailExistenteException("Email já existente");
+            }
+
+            jogs.Add(j);
+        }
+
+        public List<Jogador> Listar()
+        {
+            return jogs;
+        }
+    }
+
+    class EmailExistenteException : Exception
+    {
+        //a fazer
+    }
+
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
